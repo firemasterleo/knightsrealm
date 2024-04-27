b9fb53d69cc319b1653a0a7672df6818dbe18a74
firemasterleo <firemasterleo@outlook.com>
2024-04-27 08:24:33 +0100

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,14 +12,13 @@
     <img src="../assets/chessbackground1.jpg" alt="">
     </div>
             <div class="image2">
-    
-    <img src="../assets/chessbackground2.png" alt="">
+                <img src="../assets/chessbackground2.png" alt="">
     </div>
         </div>
 
 <div class="section2">
     <div class="widget">
-        <iframe src="https://fritz.chessbase.com" style="width:760px;height:480px"></iframe>    </div>
+        <iframe src="https://play.chessbase.com" style="width:760px;height:480px"></iframe>       </div>
 </div>
     <div class="widget">
         <iframe src="https://liveblitz.chessbase.com" width="400" height="440"></iframe></div>
@@ -32,59 +31,68 @@
     height: 100rem;
     overflow-x: hidden;
 
-    .herocontent {
-        position: absolute;
-        z-index: 15;
+    .section1 {
+        position: relative;
+        border: solid blue;
 
-        width: 22rem;
-        margin-left: 1.5rem;
-        top: 7rem;
-        h2 {
-            font-size: 34px;
-        font-weight: 600;
-        color: #cdc5c5;
-        font-family: "Fondamento", cursive;
+        .herocontent {
+            position: absolute;
+            z-index: 15;
+    
+            width: 22rem;
+            margin-left: 1.5rem;
+            top: 7rem;
+            h2 {
+                font-size: 34px;
+            font-weight: 600;
+            color: #cdc5c5;
+            font-family: "Fondamento", cursive;
+            }
+        }
+    
+        .image1 {
+            position: absolute;
+            width: 100vw;
+            height: 100vh;
+            z-index: 10;
+            overflow: hidden;
+            
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                opacity: 85%;
+                transform: scale(1.05) translateX(2%); /* Increase the scale to zoom in by 20% */
+    
+    
+            }
+        }
+    
+        .image2 {
+            
+            width: 100vw;
+            height: 100vh;
+            overflow: hidden;
+            
+            img {
+                width: 100%;
+                height: 100%;
+                margin-left: 4rem;
+                object-fit: cover;
+                opacity: 30%;
+                transform: scale(1.7) translateX(4%); /* Increase the scale to zoom in by 20% */
+    
+    
+    
+            }
         }
     }
 
-    .image1 {
-        position: absolute;
-        width: 100vw;
-        height: 100vh;
-        z-index: 10;
-        overflow: hidden;
-        
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            opacity: 85%;
-            transform: scale(1.05) translateX(2%); /* Increase the scale to zoom in by 20% */
-
-
-        }
-    }
-
-    .image2 {
-        width: 100vw;
-        height: 100vh;
-        overflow: hidden;
-        
-        img {
-            width: 100%;
-            height: 100%;
-            margin-left: 4rem;
-            object-fit: cover;
-            opacity: 30%;
-            transform: scale(1.7) translateX(4%); /* Increase the scale to zoom in by 20% */
-
-
-
-        }
-    }
 
     .section2 {
+        position: relative;
         overflow-x: auto;
+        border: solid red;
     }
 }
 
